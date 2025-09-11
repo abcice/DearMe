99df7d2d8e66ac7380ac0a404a6e9e81984a2ba4
Mohamed Meshaima <mohamedoaderu@gmail.com>
2025-09-11 16:01:56 +0300

    initiated the app and created templates

---
+++ DearMe/main_app/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView 
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+
+class Home(LoginView):
+    template_name = 'home.html'
+
+def about(request):
+    return render(request, 'about.html')
